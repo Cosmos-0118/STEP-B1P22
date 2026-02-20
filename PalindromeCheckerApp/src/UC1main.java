94cb057ca9563225756946ebb3e9e55653de3dbb
Dhanush <dhanush13244@gmail.com>
2026-02-20 09:13:31 +0530

    completed UC1 of palinderomeChecker

---
+++ PalindromeCheckerApp/src/UC1main.java
@@ -0,0 +1,7 @@
+package PalindromeCheckerApp.src;
+
+public class UC1main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to plindrome checker management system\nversion: 1.0\nSystems initialized successfully\n");
+    }
+}
